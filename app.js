d3b8b8e64ffdb5b0767ed02b17cde2c52cfc76b8
DavemskyM <metelusdavensky@gmail.com>
2019-11-05 11:56:35 -0500

    added ...

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 //setting view engine
 app.set('view engine','ejs');
 // middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 /*
 ROUTES
@@ -13,10 +15,17 @@ ROUTES
  
 
 app.get('/', function (req, res) {
-    res.render('home.ejs');
-  res.send('Hello World')
-})
- 
+  let weathernow = "The weather in" + Response.body.city + "is 70 C";
+  let err = false;
+    res.render('home.ejs',{weather:weatherNow, error:err});
+});
+
+app.post('/whatever-we-want',function(req,res){
+  console.log(req.body.city);
+  res.end();
+});
+
+
 app.listen(3000,function(){
 console.log('server is live on port: 3000');
 });
